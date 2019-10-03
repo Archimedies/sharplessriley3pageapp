05b193312a00f4c1f594cd6bc960dd51a21e2400
Archimedies <kevinbremuda@gmail.com>
2019-10-02 20:25:26 -0400

    commit

--- App.js
+++ App.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
-export default class App extends Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>Open up App.js to start working on your app!</Text>
-            </View>
-        );
-    }
+import HomeScreen from './views/Home';
+import AboutScreen from './views/About';
+import SettingsScreen from './views/Settings';
+import LoginScreen from './views/Login';
+
+const tabNavigator = createBottomTabNavigator({
+  Home: HomeScreen,
+  Login: LoginScreen,
+  About: AboutScreen,
+  Settings: SettingsScreen
+})
+
+export default createAppContainer(tabNavigator)
+
+
+class App extends Component {
+  render() {
+      return (
+          <View style={styles.container}>
+              <Text>Open up App.js to start working on your app!</Text>
+          </View>
+      );
+  }
 }
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
